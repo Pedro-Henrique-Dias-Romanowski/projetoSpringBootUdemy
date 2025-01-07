4738f20f4580c037aa3614afd363fbbf3b690ad5
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-07 17:53:18 -0300

    fix: aÃlterando a classe UserController

--- cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/Controllers/UserController.java
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/Controllers/UserController.java
@@ -2,9 +2,16 @@ package com.cursoudemy.cursoUdemy.Controllers;
 
 import com.cursoudemy.cursoUdemy.entities.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @GetMapping
     public ResponseEntity<User> findAll(){
         User u = new User(1L, "Pedro Henrique", "pedro@gmail", "12345", "pedro123");
         return ResponseEntity.ok().body(u);

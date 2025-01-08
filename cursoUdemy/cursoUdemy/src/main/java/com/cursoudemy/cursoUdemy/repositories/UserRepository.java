a68b430d5f4fcea312592c9d5b6a460f2a55aac2
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-08 16:55:41 -0300

    feat: criando a repository para manipular o banco de dados em mémoria(H2)

---
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.cursoudemy.cursoUdemy.repositories;
+
+import com.cursoudemy.cursoUdemy.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}

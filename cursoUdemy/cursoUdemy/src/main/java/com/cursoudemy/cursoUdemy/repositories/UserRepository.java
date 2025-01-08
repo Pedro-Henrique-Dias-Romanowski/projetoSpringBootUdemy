e17f6f00b56dd617f2a587e43897971559ecf697
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-08 17:40:59 -0300

    delete: removendo a anotação de @repository da interface já que não é necessário

--- cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/repositories/UserRepository.java
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.cursoudemy.cursoUdemy.repositories;
 import com.cursoudemy.cursoUdemy.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }

35987e9f18699ab202e5aceea26a55ddfe02b04a
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-08 17:40:10 -0300

    feat: adicionado método de buscar ppelo id e mapeando a url dos endpoints

--- cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/Controllers/UserController.java
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/Controllers/UserController.java
@@ -1,19 +1,45 @@
 package com.cursoudemy.cursoUdemy.Controllers;
 
 import com.cursoudemy.cursoUdemy.entities.User;
+import com.cursoudemy.cursoUdemy.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Pedro Henrique", "pedro@gmail", "12345", "pedro123");
-        return ResponseEntity.ok().body(u);
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<User>> findAll(){
+
+        try{
+            List<User> listUsers = service.findAll();
+            return listUsers != null ?  ResponseEntity.ok().body(listUsers)  : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        try {
+            Optional<User> user = service.findById(id);
+            return ResponseEntity.ok().body(user.get());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 }

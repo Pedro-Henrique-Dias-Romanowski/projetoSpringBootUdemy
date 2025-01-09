9b4e284fd4bdcffebef519d0b063d90b33c514de
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-09 16:52:46 -0300

    feat: criando a camada de serviço da order

---
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.cursoudemy.cursoUdemy.services;
+
+
+import com.cursoudemy.cursoUdemy.entities.Order;
+import com.cursoudemy.cursoUdemy.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> order = repository.findById(id);
+        return order.get();
+    }
+}

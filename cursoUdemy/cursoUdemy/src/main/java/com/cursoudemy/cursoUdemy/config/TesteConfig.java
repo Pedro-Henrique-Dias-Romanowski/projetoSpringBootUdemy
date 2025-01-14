8b6a65bd32ac705ea368048bfd8a368be2e08f3c
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-14 15:21:40 -0300

    fix: fazendo o mock dos OrdersItems

--- cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/config/TesteConfig.java
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/config/TesteConfig.java
@@ -1,15 +1,9 @@
 package com.cursoudemy.cursoUdemy.config;
 
 
-import com.cursoudemy.cursoUdemy.entities.Category;
-import com.cursoudemy.cursoUdemy.entities.Order;
-import com.cursoudemy.cursoUdemy.entities.Product;
-import com.cursoudemy.cursoUdemy.entities.User;
+import com.cursoudemy.cursoUdemy.entities.*;
 import com.cursoudemy.cursoUdemy.entities.enums.OrderStatus;
-import com.cursoudemy.cursoUdemy.repositories.CategoryRepository;
-import com.cursoudemy.cursoUdemy.repositories.OrderRepository;
-import com.cursoudemy.cursoUdemy.repositories.ProductRepository;
-import com.cursoudemy.cursoUdemy.repositories.UserRepository;
+import com.cursoudemy.cursoUdemy.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TesteConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -61,10 +58,16 @@ public class TesteConfig implements CommandLineRunner {
         p4.getCategories().add(cat3);
         p5.getCategories().add(cat2);
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 
 

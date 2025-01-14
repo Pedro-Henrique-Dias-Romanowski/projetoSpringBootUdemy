160e86431531a2a2b041db01bbbfed04e70da46a
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-14 16:32:17 -0300

    feat: adicionando a relação entre produto e pedido, agora dentro de pÃedidos, podemos ver os produtos associados a ele

--- cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/entities/Product.java
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/entities/Product.java
@@ -35,6 +35,10 @@ public class Product implements Serializable {
                 inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
 
@@ -88,6 +92,16 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem orderItem : items){
+            set.add(orderItem.getOrder());
+        }
+        return set;
     }
 
     @Override

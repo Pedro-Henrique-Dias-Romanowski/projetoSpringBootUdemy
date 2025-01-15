ab7e2381fb311443894a841e2466ba86a611f239
Pedro Henrique <pedro.romanowksi02@gmail.com>
2025-01-15 16:48:08 -0300

    feat: adicionando os métodos de salvar e deletar o usuário no banco de dados

--- cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/Controllers/UserController.java
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/Controllers/UserController.java
@@ -5,10 +5,7 @@ import com.cursoudemy.cursoUdemy.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +20,6 @@ public class UserController {
 
     @GetMapping("/findAll")
     public ResponseEntity<List<User>> findAll(){
-
         try{
             List<User> listUsers = service.findAll();
             return listUsers != null ?  ResponseEntity.ok().body(listUsers)  : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
@@ -42,4 +38,37 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
+
+    @PostMapping("/save")
+    public ResponseEntity<User> save(@RequestBody User user){
+        try{
+            user = service.save(user);
+            return user != null ? ResponseEntity.status(HttpStatus.CREATED).body(user) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        try{
+            service.delete(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+//        try{
+//            User newUser = service.save(user);
+//            newUser.setId(id);
+//            return ResponseEntity.ok().body(newUser);
+//        } catch (Exception e){
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+//        }
+//    }
 }

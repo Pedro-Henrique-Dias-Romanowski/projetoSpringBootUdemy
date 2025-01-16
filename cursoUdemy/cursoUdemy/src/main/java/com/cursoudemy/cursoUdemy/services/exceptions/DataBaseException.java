c2ba397a062099d643f872910489c21321cad190
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-16 18:09:26 -0300

    feat: criando exceção para tratamento relacionado com banco de dados

---
+++ cursoUdemy/cursoUdemy/src/main/java/com/cursoudemy/cursoUdemy/services/exceptions/DataBaseException.java
@@ -0,0 +1,7 @@
+package com.cursoudemy.cursoUdemy.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
